15df2ca6b098d9b55a0d5551cf538aaac47e7e03
Attila Füzes <attila.fuzes@cs.utcluj.ro>
2024-11-21 09:14:06 +0200

    Create get-i2c-devices.ino

---
+++ get-i2c-devices.ino
@@ -0,0 +1,51 @@
+#include <Wire.h>
+
+void setup() {
+  // ESP32 boards have a delay with UART for some reason from a reset after
+  // uploading new code to the development board
+#if 1  // board was newly programmed
+  delay(1000);
+#else
+  // https://www.arduino.cc/reference/en/language/functions/communication/serial/ifserial/
+  while (!Serial) {
+    ;  // wait for the serial port to connect. Needed for native USB
+  }
+#endif  
+
+  // Initialize I²C
+  Wire.begin();
+
+  // Search for all devices on the bus
+  printAllI2CDevicesOnBus();
+}
+
+void loop() {}
+
+void printAllI2CDevicesOnBus() {
+  uint8_t deviceCounter;
+
+  Serial.println("Scanning for devices");
+  Serial.println("--------------------");
+
+  // Scan all addresses from 1 to 126
+  for (uint8_t i = 1; i < 127; i++) {
+    Wire.beginTransmission(i);
+
+    // Check for error at address i
+    const uint8_t error = Wire.endTransmission();
+
+    if (error == 0) {
+      // If there is no error, print the address
+      Serial.printf("I²C device found at address 0x%02x\n", i);
+      deviceCounter++;
+    } else if (error == 4) {
+      // This is in case of a faulty device
+      Serial.printf("Unknown error at address 0x%02x\n", i);
+    }
+  }
+
+  if (deviceCounter == 0)
+    Serial.println("No I²C devices found");
+  else
+    Serial.printf("Found %d device(s) in total\n", deviceCounter);
+}
